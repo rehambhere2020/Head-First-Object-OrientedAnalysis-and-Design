6217087360808e767c3a3fe654a9ddcbf3724f5c
rehambhere2020 <rehambhere2020@gmail.com>
2022-04-19 14:38:37 +0300

    add abstract and encupsulation lesson

---
+++ abstract and encupsulation/src/com/oopconcept/Main.java
@@ -0,0 +1,9 @@
+package com.oopconcept;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("abstract an dencupsulation");
+    }
+}
---
+++ abstract and encupsulation/src/com/oopconcept/Plane.java
@@ -0,0 +1,5 @@
+package com.oopconcept;
+
+public class Plane {
+
+}
